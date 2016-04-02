3b7611e625e78899397d1b1df498224b43fcc216
Gardent Marc <marcgardent@yahoo.fr>
2016-04-02 19:40:20 +0200

    Prepare new sgf properties

--- Source/IGOEnchi.GoGameLib/GameInfo.cs
+++ Source/IGOEnchi.GoGameLib/GameInfo.cs
@@ -26,5 +26,43 @@ namespace IGOEnchi.GoGameLogic.Models
         public int Handicap { get; set; }
 
         public float Komi { get; set; }
+
+        /// <summary>
+        /// Name of game
+        /// Todo impl  : map to GN
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// some background information and/or to summarize the game itself
+        /// TODO impl : map to GC
+        /// </summary>
+        public string Summarize { get; set; }
+
+
+
+        /// <summary>
+        /// TODO impl : map to DT
+        /// </summary>
+        public string DateOfGame { get; set; }
+
+
+        /// <summary>
+        /// TODO impl : map to RU
+        /// </summary>
+        public string Rules { get; set; }
+
+
+        /// <summary>
+        /// TODO impl : map to AP [name:version]
+        /// the name of the application used to create this gametree
+        /// </summary>
+        public string ProgramName { get; set; }
+
+        /// <summary>
+        /// TODO impl : map to AP [name:version]
+        ///  version number of the application used to create this gametree
+        /// </summary>
+        public string ProgramVersion { get; set; }
     }
 }
